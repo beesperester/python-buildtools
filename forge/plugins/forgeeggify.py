2ab652f45cf2f595c0f8700739de4f1dff35b4ed
Bernhard Esperester <bernhard@esperester.de>
2018-07-05 15:32:29 +0200

    remove existing eggs

--- forge/plugins/forgeeggify.py
+++ forge/plugins/forgeeggify.py
@@ -3,16 +3,44 @@
 # system
 import os
 import subprocess
+import re
 
 # fs
 from fs.osfs import OSFS
 from fs.copy import copy_fs, copy_file
 
 # buildtools
-from forge.utils.fs_utils import assertFS, emptyFS
+from forge.utils.fs_utils import assertFS
 
 class MissingDestinationException(Exception):
     """Missing destination exception."""
+
+def stripNumbers(string):
+    """ Strip numbers from string.
+
+    Args:
+        string (basestring)
+    """
+
+    return re.sub(r"([0-9:\.]+)", "", string)
+
+def removeOldEggs(fs, name):
+    """ Remove old eggs.
+        Strip version numbers from name string
+        and remove matching existing eggs.
+
+    Args:
+        fs (fs)
+        name (basestring)
+    """
+
+    for destName in fs.listdir(unicode("/")):
+        if destName.endswith(".egg"):
+            destNameStripped = stripNumbers(destName)
+            nameStripped = stripNumbers(name)
+            
+            if destNameStripped == nameStripped:
+                fs.remove(unicode(destName))
 
 def eggifySingle(srcFS, src, destFS, dest):
     """ Eggify single source to single destination.
@@ -38,15 +66,23 @@ def eggifySingle(srcFS, src, destFS, dest):
         assertFS(destFS.getsyspath(unicode(dest)))
 
         workingDir = srcFS.getsyspath(unicode("/"))
+        devnull = open(os.devnull, 'w')
 
-        subprocess.check_call(["python", src, "bdist_egg"], cwd=workingDir)
+        subprocess.check_call(["python", src, "bdist_egg"], cwd=workingDir, stdout=devnull, stderr=devnull)
     
         if srcFS.isdir(unicode("dist")):
             distFS = srcFS.opendir(unicode("dist"))
 
             for name in reversed(sorted(distFS.listdir("/"))):
                 if name.endswith(".egg"):
-                    copy_file(distFS, unicode(name), destFS, unicode("{}/{}".format(dest, name)))
+                    destEggFS = destFS.opendir(unicode(dest))
+
+                    # remove existing eggs
+                    removeOldEggs(destEggFS, name)
+
+                    print "copy {} to {}".format(distFS.getsyspath(unicode(name)), destEggFS.getsyspath(unicode(name)))
+
+                    copy_file(distFS, unicode(name), destEggFS, unicode(name))
 
                     break
 
